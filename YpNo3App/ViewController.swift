9e730f8d5abc52c7f7b01eb8d749b6c9268e0d86
yoppen <yopppen.4403@gmail.com>
2021-06-21 13:57:32 +0900

    Initial Commit

---
+++ YpNo3App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YpNo3App
+//
+//  Created by 山岸善将 on 2021/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
